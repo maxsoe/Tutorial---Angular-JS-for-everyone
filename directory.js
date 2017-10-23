5fc283573de177e3f5d5d784d1e1663dc932bb00
Max Soe <maxsoe@gmail.com>
2017-10-23 19:57:27 +0100

    Tutorial #5 - Adding A Form Event

--- directory.js
+++ directory.js
@@ -4,6 +4,12 @@ angular.module('directoryApp', [])
       {name: 'Scott', age: 29},
       {name: 'Ross', age: 31},
       {name: 'Ben', age: 32},
-       {name: 'Courtney', age: 29}
-     ]
+      {name: 'Courtney', age: 29}
+    ];
+
+    $scope.addPerson = function() {
+      $scope.list.push({name:$scope.name, age: $scope.age});
+      $scope.name ='';
+      $scope.age = 0;
+    };
   });
